cfcb2424034aca4ae9ef4dafe490e3c4000998c9
Zos <rj7555555@gmail.com>
2022-01-15 11:48:54 +0530

    card transition

--- src/s30/card.js
+++ src/s30/card.js
@@ -9,14 +9,12 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    transition:'0.5s',
     "&:hover":{
-        // backgroundColor:'blue',
         marginTop:'-10px',
-        border: '1px solid transparent',
-        borderImage: 'linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)',
+        border: '2px solid transparent',
         borderImageSlice: '1',
         animation:'$myEffect 3000ms linear infinite',
-       
     }
   },
   "@keyframes myEffect": {
@@ -24,47 +22,14 @@ const useStyles = makeStyles({
         borderImage: 'linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)',
         borderImageSlice: '1',
     },
-    "10%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 10%, #2c90fc 35%, #b8fd33 60%, #fec837 85%, #fd1892 10%)',
-        borderImageSlice: '1',
-    },
-    "20%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 20%, #2c90fc 45%, #b8fd33 70%, #fec837 95%, #fd1892 20%)',
-        borderImageSlice: '1',
-    },
-    "30%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 30%, #2c90fc 55%, #b8fd33 80%, #fec837 5%, #fd1892 30%)',
-        borderImageSlice: '1',
-    },
-    "40%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 40%, #2c90fc 65%, #b8fd33 90%, #fec837 15%, #fd1892 40%)',
-        borderImageSlice: '1',
-    },
     "50%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 50%, #2c90fc 75%, #b8fd33 100%, #fec837 25%, #fd1892 50%)',
+        borderImage: 'linear-gradient(45deg,#fb0094,#00f,#0f0,#ff0,#f00,#fb0094,#00f,#0f0,#ff0,#f00)',
         borderImageSlice: '1',
     },
-    "60%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 60%, #2c90fc 85%, #b8fd33 10%, #fec837 35%, #fd1892 60%)',
+    "100%": {
+        borderImage: 'linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)',
         borderImageSlice: '1',
-    },
-    "70%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 70%, #2c90fc 95%, #b8fd33 20%, #fec837 45%, #fd1892 70%)',
-        borderImageSlice: '1',
-    },
-    "80%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 80%, #2c90fc 5%, #b8fd33 30%, #fec837 55%, #fd1892 80%)',
-        borderImageSlice: '1',
-    },
-    "90%": {
-        borderImage: 'linear-gradient(to bottom right, #b827fc 90%, #2c90fc 15%, #b8fd33 40%, #fec837 65%, #fd1892 90%)',
-        borderImageSlice: '1',
-    },
-    
-    // "100%": {
-    //     borderImage: 'linear-gradient(to bottom left, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)',
-    //     borderImageSlice: '1',
-    // }
+    }
   },
   bullet: {
     display: 'inline-block',
